7e7f916b916794e7023ce2333ec9b3ea56ad9024
Felipe Gaete Alvardo <felipegaetealvardo@MacBook-Air-de-Felipe.local>
2022-08-04 22:44:58 -0400

    Se agrega conexión con REACT

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.9 
-# Or any preferred Python version.
-ADD main.py /
-# install dotenv n shits
-RUN pip install requests beautifulsoup4 python-dotenv
-# install fastapi and uvicorn
-RUN pip install fastapi uvicorn
-#CMD [ "python", "./main.py" ]
-# Or enter the name of your unique directory and parameter set.
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.consoles import consola
 
 app = FastAPI(
     title="APIS de Sistema de Colección"
 )
 
+origins = ['https://localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(consola)
